1abe824fca9d5f6b00880d45dd63c1a4a34f1582
Synthasmagoria <50552091+Synthasmagoria@users.noreply.github.com>
2020-03-05 10:48:20 +0100

    Initial commit

---
+++ Source/Overcast/OvercastGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OvercastGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVERCAST_API AOvercastGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
